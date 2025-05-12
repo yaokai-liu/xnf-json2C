56cc67ed2bfc8a1e1d09a5341c98194bc24ea3cf
Yaokai Liu <yaokai.liu@foxmail.com>
2025-05-12 10:49:16 +0800

    generator for LIU-CC.

---
+++ codegen.py
@@ -0,0 +1,75 @@
+import sys
+from pathlib import Path
+from string import Template as Tp
+from Generator import Generator, gen_token_enum, gen_token_name
+LICENSE_TPL = """/**
+ * License
+ *
+ * ${projectDescription}
+ * Copyright (C) 2024 Yaokai Liu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Project Name: ${projectName}
+ * Module Name: ${moduleName}
+ * Filename: ${filename}
+ * Copyright (c) 2024 Yaokai Liu. All rights reserved.
+ **/
+"""
+if __name__ == '__main__':
+    JSON_DIR = Path(sys.argv[1])
+    TEMPLATE_DIR = Path(sys.argv[2])
+    OUT_DIR = Path(sys.argv[3])
+
+    myLicense = Tp(LICENSE_TPL).substitute(
+        projectDescription="LIU-CC - A Compiler for a Custom C like Programming Language",
+        projectName="LIU-CC",
+        moduleName="grammar",
+        filename="${filename}",
+    )
+
+    GParser = Generator(json_dir=JSON_DIR / "parse",
+                         template_dir=TEMPLATE_DIR / "parse",
+                         out_dir=OUT_DIR / "parse",
+                         target="Program")
+    GPreprocessor = Generator(json_dir=JSON_DIR / "preprocess",
+                              template_dir=TEMPLATE_DIR / "preprocess",
+                              out_dir=OUT_DIR / "preprocess",
+                              target="PreprocessCommand")
+    GParser.set_license(Tp(myLicense).substitute(filename="${filename}"))
+    GPreprocessor.set_license(Tp(myLicense).substitute(filename="${filename}"))
+
+
+    tokens = sorted(set(GParser.tokens) | set(GPreprocessor.tokens))
+    GParser.set_extend_tokens(tokens)
+    GPreprocessor.set_extend_tokens(tokens)
+
+    myLicense = Tp(myLicense).substitute(filename="${filename}")
+
+    gen_token_enum(TEMPLATE_DIR / "tokens.h.tpl",
+                   Tp(myLicense).substitute(filename="tokens.gen.h"),
+                   tokens,
+                   OUT_DIR / "tokens.gen.h")
+
+    gen_token_name(TEMPLATE_DIR / "tokens.c.tpl",
+                   Tp(myLicense).substitute(filename="tokens.gen.c"),
+                   tokens,
+                   OUT_DIR / "tokens.gen.c")
+
+    GParser.set_context("ParserContext")
+    GParser.set_rule_prefix("Parse")
+    GPreprocessor.set_context("TokenizerContext")
+    GPreprocessor.set_rule_prefix("Preprocess")
+    GParser.generate()
+    GPreprocessor.generate()
