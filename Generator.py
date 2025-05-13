8e8c0a2e52cf5a5aa5c6d2761aa106ecb9a82c81
Yaokai Liu <yaokai.liu@foxmail.com>
2025-05-13 20:07:43 +0800

    pass ErrInfo to reduce rules.

--- Generator.py
+++ Generator.py
@@ -154,7 +154,7 @@ class Generator:
 
     def gen_rules(self):
         rule_names = self.rules.keys()
-        args = f"(Token argv[], {self.context} *, const Allocator * allocator);"
+        args = f"(Token argv[], {self.context} *, ErrInfo *, const Allocator * allocator);"
         enum_reduces = sorted(f"{self.rule_to_enum(r)} = {i}" for i, r in enumerate(rule_names))
         rules = sorted(f"{self.rule_target(r)} * {self.rule_to_name(r)} {args};" for r in rule_names)
         assign_reduces = sorted([f"[{self.rule_to_enum(r)}] = (fn_{self.rule_prefix.lower()}_reduce *) {self.rule_to_name(r)}" for r in rule_names])
