c90c126a5e060f9eba9ae880ce4096e6e70fa2e8
Yaokai Liu <yaokai.liu@foxmail.com>
2025-05-29 21:58:19 +0800

    token and codegen for xLR.

---
+++ DATA.py
@@ -0,0 +1,49 @@
+TERMINALS = {
+    "QUANTIFIER": 0,
+
+    "IF": "if",
+    "FOR": "for",
+    "ELSE": "else",
+    "TOKEN": "token",
+    "WHILE": "while",
+
+    "NUMBER": 0,
+    "IDENTIFIER": 0,
+    "BUILTIN_TYPE": 0,
+    "BUILTIN_VARIABLE": 0,
+
+    "BOOL_OR": "||",
+    "BOOL_NOT": "!",
+    "BOOL_AND": "&&",
+
+    "DOT": ".",
+    "COMMA": ",",
+    "ASSIGNER": "=",
+    "SEMICOLON": ";",
+    "LEFT_BRACKET": "{",
+    "RIGHT_BRACKET": "}",
+    "LEFT_PARENTHESIS": "(",
+    "RIGHT_PARENTHESIS": ")",
+    "LEFT_SQUARE_BRACKET": "[",
+    "RIGHT_SQUARE_BRACKET": "]",
+
+
+    "COND_SIN_OP": 0,
+    "COND_BIN_OP": 0,
+
+    "ARITH_0_BIN_OP": 0,
+    "ARITH_1_BIN_OP": 0,
+    "ARITH_2_BIN_OP": 0,
+    "ARITH_3_BIN_OP": 0,
+
+    "ARITH_0_SIN_OP": 0,
+    "ARITH_1_SIN_OP": 0,
+    "ARITH_2_SIN_OP": 0,
+    "ARITH_3_SIN_OP": 0,
+    "ARITH_4_SIN_OP": 0,
+
+    "INTEGRATED_AHEAD_OP": 0,
+    "INTEGRATED_FOLLOW_OP": 0,
+
+    "TERMINATOR": 0
+}
---
+++ codegen.py
@@ -0,0 +1,67 @@
+import sys
+from pathlib import Path
+from string import Template as Tp
+from Generator import Generator, gen_token_enum, gen_token_name
+from DATA import TERMINALS
+LICENSE_TPL = """/**
+ * License
+ *
+ * ${projectDescription}
+ * Copyright (C) 2024 Yaokai Liu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Project Name: ${projectName}
+ * Module Name: ${moduleName}
+ * Filename: ${filename}
+ * Copyright (c) 2024 Yaokai Liu. All rights reserved.
+ **/
+"""
+if __name__ == '__main__':
+    JSON_DIR = Path(sys.argv[1])
+    TEMPLATE_DIR = Path(sys.argv[2])
+    OUT_DIR = Path(sys.argv[3])
+
+    myLicense = Tp(LICENSE_TPL).substitute(
+        projectDescription="xLR - Dynamic LR(1) Grammar Parser",
+        projectName="xLR",
+        moduleName="grammar",
+        filename="${filename}",
+    )
+
+    GXLR = Generator(json_dir=JSON_DIR / "xLR",
+                       template_dir=TEMPLATE_DIR / "xLR",
+                       out_dir=OUT_DIR / "xLR",
+                       target="GrammarEntry")
+    GXLR.set_license(Tp(myLicense).substitute(filename="${filename}"))
+    tokens = sorted(set(GXLR.tokens) | set(TERMINALS.keys()))
+    GXLR.set_extend_tokens(tokens)
+
+    myLicense = Tp(myLicense).substitute(filename="${filename}")
+
+    gen_token_enum(TEMPLATE_DIR / "tokens.h.tpl",
+                   Tp(myLicense).substitute(filename="tokens.gen.h"),
+                   "XLR",
+                   tokens,
+                   OUT_DIR / "tokens.gen.h")
+
+    gen_token_name(TEMPLATE_DIR / "tokens.c.tpl",
+                   Tp(myLicense).substitute(filename="tokens.gen.c"),
+                   "XLR",
+                   tokens,
+                   OUT_DIR / "tokens.gen.c")
+
+    GXLR.set_context("LRContext")
+    GXLR.set_prefix("XLR")
+    GXLR.generate()
