ed934f45340efd66d8e9a8def92916ace63d01dc
Yaokai Liu <yaokai.liu@foxmail.com>
2025-06-09 18:12:31 +0800

    context prefix.

---
+++ DATA.py
@@ -0,0 +1,19 @@
+TERMINALS = {
+    "DOT": ".",
+    "COMMA": ",",
+    "LEFT_BRACKET": "{",
+    "RIGHT_BRACKET": "}",
+    "LEFT_SQUARE_BRACKET": "[",
+    "RIGHT_SQUARE_BRACKET": "]",
+
+    "ASSIGN": 0,
+
+    "NULL": 0,
+    "BOOLEAN": 0,
+
+    "KEY": 0,
+    "TEXT": 0,
+    "NUMBER": 0,
+
+    "TERMINATOR": 0,
+}
