bcdf579be015f2f240e17094f5ba8e796c31e1a8
Yaokai Liu <yaokai.liu@foxmail.com>
2026-03-17 21:07:33 +0800

    new project Mathlang.

---
+++ DATA.py
@@ -0,0 +1,53 @@
+TERMINALS = {
+    "DEFINITION_TYPE": 0, # "Definition", "Convention",
+    "THEOREM_TYPE": 0, # "proposition", "Conclusion", "Theorem", "Lemma",
+    "AXIOM_TYPE": 0, # "Axiom" "Assumption",
+    "PROCEDURE": "Procedure",
+    "DENOTEAS": "Denoteas",
+    "PROOF": "Proof",
+    "LATEX": "Latex",
+
+    "Notation": 0,
+    "NotationBlock": 0,
+    "LatexExpression": 0,
+    "NotationDefinitions": 0,
+
+
+    "DE": 0,
+    "THE": 0,
+    "NOUN": 0,
+    "VERB": 0,
+    "NUMBER": 0,
+    "PRONOUN": 0,
+    "ADVERB": 0,
+    "ADJECTIVE": 0,
+    "PREP": 0,
+    "CONJUNCTION": 0,
+    "QUANTIFIER": 0,
+    "ENTRY_TYPE": 0,
+
+    "DOT": ".",
+    "NOT": "!",
+    "COMMA": ",",
+    "COLON": ":",
+    "ASSIGNER": "=",
+    "SEMICOLON": ";",
+    "VERTICAL_BAR": "|",
+    "DOUBLE_COLON": "::",
+    "QUESTION_MARK": "?",
+
+    "LEFT_BRACKET": "{",
+    "RIGHT_BRACKET": "}",
+    "LEFT_PARENTHESIS": "(",
+    "RIGHT_PARENTHESIS": ")",
+    "LEFT_ANGLE_BRACKET": "<",
+    "RIGHT_ANGLE_BRACKET": ">",
+    "LEFT_SQUARE_BRACKET": "[",
+    "RIGHT_SQUARE_BRACKET": "]",
+    "DOUBLE_LEFT_SQUARE_BRACKET": "[[",
+    "DOUBLE_RIGHT_SQUARE_BRACKET": "]]",
+
+    "TERMINATOR": 0
+}
+TYPES = {
+}
---
+++ codegen.py
@@ -0,0 +1,72 @@
+import sys
+from pathlib import Path
+from string import Template as Tp
+from Generator import Generator, gen_token_enum, gen_token_name
+from DATA import *
+
+LICENSE_TPL = """/**
+ * License
+ *
+ * ${projectDescription}
+ * Copyright (C) ${year} Yaokai Liu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Project Name: ${projectName}
+ * Module Name: ${moduleName}
+ * Filename: ${filename}
+ * Copyright (c) ${year} Yaokai Liu. All rights reserved.
+ **/
+"""
+
+if __name__ == '__main__':
+    JSON_DIR = Path(sys.argv[1])
+    TEMPLATE_DIR = Path(sys.argv[2])
+    OUT_DIR = Path(sys.argv[3])
+
+    myLicense = Tp(LICENSE_TPL).substitute(
+        projectDescription="Mathlang - A Practical Language for Mathematics",
+        projectName="Mathlang",
+        moduleName="grammar",
+        filename="${filename}",
+        year="2026",
+    )
+
+    G_MATHLANG = Generator(json_dir=JSON_DIR / "Mathlang",
+                       template_dir=TEMPLATE_DIR / "Mathlang",
+                       out_dir=OUT_DIR / "Mathlang",
+                       target="GrammarEntry")
+    G_MATHLANG.set_license(Tp(myLicense).substitute(filename="${filename}"))
+    tokens = sorted(set(G_MATHLANG.tokens) | set(TERMINALS.keys()))
+    G_MATHLANG.set_extend_tokens(tokens)
+
+    myLicense = Tp(myLicense).substitute(filename="${filename}")
+
+    gen_token_enum(TEMPLATE_DIR / "tokens.h.tpl",
+                   Tp(myLicense).substitute(filename="tokens.gen.h"),
+                   "MATHLANG",
+                   tokens,
+                   OUT_DIR / "tokens.gen.h")
+
+    gen_token_name(TEMPLATE_DIR / "tokens.c.tpl",
+                   Tp(myLicense).substitute(filename="tokens.gen.c"),
+                   "MATHLANG",
+                   tokens,
+                   OUT_DIR / "tokens.gen.c")
+
+    G_MATHLANG.set_context("MLContext")
+    G_MATHLANG.set_token_prefix("MATHLANG")
+    G_MATHLANG.set_prefix("MATHLANG")
+    G_MATHLANG.set_types(TYPES)
+    G_MATHLANG.generate()
